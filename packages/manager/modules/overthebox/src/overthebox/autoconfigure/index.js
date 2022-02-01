56dfab427fb4402a2859b25ee98e55a86bc97ef4
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2022-02-01 11:07:01 +0100

    feat(overthebox.autoconfig): add multipath configuration
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/modules/overthebox/src/overthebox/autoconfigure/index.js
+++ packages/manager/modules/overthebox/src/overthebox/autoconfigure/index.js
@@ -3,10 +3,12 @@ import routing from './overTheBox-autoconfigure.routing';
 
 import dhcp from './dhcp';
 
+import multipath from './multipath';
+
 const moduleName = 'ovhManagerOtbAutoconfigure';
 
 angular
-  .module(moduleName, ['ui.router', 'ovh-api-services', dhcp])
+  .module(moduleName, ['ui.router', 'ovh-api-services', dhcp, multipath])
   .component('overTheBoxAutoconfigure', component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
